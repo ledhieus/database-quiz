be5e42d79e958a44463b4eb48f084fa06122910e
ledhieus <ledan0602@gmail.com>
2024-10-26 17:42:24 +0700

    database

--- server.js
+++ server.js
@@ -2,16 +2,12 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("database.json"); // Tệp JSON chứa dữ liệu
 const middlewares = jsonServer.defaults();
+const PORT = process.env.PORT || 8000;
 
 server.use(middlewares);
-server.use(jsonServer.bodyParser);
 
 // Sử dụng các phương thức đầy đủ
 server.use(router);
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log("JSON Server is running");
-});
 
-module.exports = server
+server.listen(PORT);
